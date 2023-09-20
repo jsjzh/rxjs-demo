507b16d50c92f9398cfdf64d9e941e4759034c41
jinzhehao <jinzhehao@souche.com>
2023-09-20 11:31:12 +0800

    feat: 完成 dragVideo

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@
 // import "./pages/bmi";
 // import "./pages/history";
 // import "./pages/editor";
-import "./pages/dragVideo";
+// import "./pages/dragVideo";
+import "./pages/operators";
--- src/pages/dragVideo/index.ts
+++ src/pages/dragVideo/index.ts
@@ -1,13 +1,80 @@
 import { html, render } from "lit-html";
-import { of } from "rxjs";
+import {
+  concatAll,
+  filter,
+  fromEvent,
+  map,
+  takeUntil,
+  withLatestFrom,
+} from "rxjs";
+
+import "./index.scss";
 
 const $document = document;
-const $app = document.getElementById("app") as HTMLDivElement;
+const $app = $document.getElementById("app") as HTMLDivElement;
 
-const template = html`<div></div>`;
+const page = html` <div id="anchor">
+  <div
+    class="video"
+    id="video"
+  >
+    <div class="masker"></div>
+    <video
+      width="100%"
+      controls
+    >
+      <source
+        src="http://download.blender.org/peach/bigbuckbunny_movies/big_buck_bunny_480p_stereo.ogg"
+        type="video/ogg"
+      />
+      Your browser does not support HTML5 video.
+    </video>
+  </div>
+</div>`;
 
-render(template, $app);
+render(page, $app);
 
-of("hello world").subscribe((value) => {
-  $app.innerText = value;
-});
+const $video = $document.getElementById("video")!;
+const $anchor = $document.getElementById("anchor")!;
+
+const scroll$ = fromEvent($document, "scroll");
+
+scroll$
+  .pipe(map((e) => $anchor.getBoundingClientRect().bottom < 0))
+  .subscribe((bool) => {
+    if (bool) {
+      $video.classList.add("video-fixed");
+    } else {
+      $video.classList.remove("video-fixed");
+    }
+  });
+
+const mouseDown$ = fromEvent<MouseEvent>($video, "mousedown");
+const mouseUp$ = fromEvent<MouseEvent>($document, "mouseup");
+const mouseMove$ = fromEvent<MouseEvent>($document, "mousemove");
+
+const validValue = (value, max, min) => {
+  return Math.min(Math.max(value, min), max);
+};
+
+mouseDown$
+  .pipe(
+    filter((e) => $video.classList.contains("video-fixed")),
+    map((e) =>
+      mouseMove$.pipe(
+        map((e) => e),
+        takeUntil(mouseUp$),
+      ),
+    ),
+    concatAll(),
+    withLatestFrom(mouseDown$, (move, down) => {
+      return {
+        x: validValue(move.clientX - down.offsetX, window.innerWidth - 320, 0),
+        y: validValue(move.clientY - down.offsetY, window.innerHeight - 180, 0),
+      };
+    }),
+  )
+  .subscribe((pos) => {
+    $video.style.left = pos.x + "px";
+    $video.style.top = pos.y + "px";
+  });
